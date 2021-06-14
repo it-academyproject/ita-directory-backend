5e2c3e949c07f25ee8d61f07d0f536749a4c97ea
JC <jcfg67@outlook.com>
2021-06-14 22:53:48 +0200

    adding Redis to save refresh token

--- app/utils/utils.js
+++ app/utils/utils.js
@@ -1,3 +1,11 @@
+const JWT = require("jsonwebtoken");
+const Hashids = require('hashids');
+const client = require('../utils/initRedis');
+const { promisify } = require("util");
+
+const hashids = new Hashids(process.env.HASH_ID_SECRET, 10);
+
+
 // const toFile = require("data-uri-to-file");
 // const Blob = require("cross-blob");
 
@@ -15,7 +23,33 @@
 **/
 const apiResponse = ({ message = '', data = {}, errors = [] }) => { return { message, data, errors } }
 
+// const signToken = (userid, maxAge = "15m") => {
+const signToken = (userid, maxAge = "2m") => {
+	const hashedId = hashids.encode(userid);
+	const payload = {iss: "itacademy", sub: { user_id: hashedId	}};
+	const secret = process.env.JWT_SECRET;
+	const options = { expiresIn: maxAge };
+	return JWT.sign(payload, secret, options);
+}
+	
+// const signRefreshToken = async (userid, maxAge = "1d") => {
+const signRefreshToken = async (userid, maxAge = "4m") => {
+	const hashedId = hashids.encode(userid);
+	const payload = {iss: "itacademy", sub: { user_id: hashedId	}};
+	const secret = process.env.JWT_REFRESH_TOKEN_SECRET;
+	const options = { expiresIn: maxAge };
+	const token = JWT.sign(payload, secret, options);
+	const set = promisify(client.set).bind(client);
+	// await set(hashedId, token, 'EX', maxAge);
+	// await set(hashedId, token, 'EX', 24 * 60 * 60);
+	await set(hashedId, token, 'EX', 4 * 60);
+	return token
+}
+	
+
 module.exports = {
 	// generateBlob,
-	apiResponse
+	apiResponse,
+	signToken,
+	signRefreshToken
 };
