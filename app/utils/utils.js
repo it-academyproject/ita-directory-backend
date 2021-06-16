70bac4075079fc5989e6f523cb29e95892a73287
Kevin Mamaqi <kevinmamaqi@gmail.com>
2021-06-16 10:56:20 +0200

    update redis client

--- app/utils/utils.js
+++ app/utils/utils.js
@@ -1,10 +1,8 @@
 const JWT = require("jsonwebtoken");
-const Hashids = require('hashids');
-const client = require('../utils/initRedis');
-const { promisify } = require("util");
+const Hashids = require("hashids");
+const {getRedisClient} = require("../utils/initRedis");
 
 const hashids = new Hashids(process.env.HASH_ID_SECRET, 10);
-
 
 // const toFile = require("data-uri-to-file");
 // const Blob = require("cross-blob");
@@ -15,41 +13,40 @@ const hashids = new Hashids(process.env.HASH_ID_SECRET, 10);
 // 	});
 // };
 
-
 /*
 / message - A user friendly message of what happened, string, defaults to ''
 / data - The main data, defaults to an object, it can be any type
 / errors - An array of errors generated in processing, defaults to []
 **/
-const apiResponse = ({ message = '', data = {}, errors = [] }) => { return { message, data, errors } }
+const apiResponse = ({message = "", data = {}, errors = []}) => {
+	return {message, data, errors};
+};
 
 // const signToken = (userid, maxAge = "15m") => {
 const signToken = (userid, maxAge = "2m") => {
 	const hashedId = hashids.encode(userid);
-	const payload = {iss: "itacademy", sub: { user_id: hashedId	}};
+	const payload = {iss: "itacademy", sub: {user_id: hashedId}};
 	const secret = process.env.JWT_SECRET;
-	const options = { expiresIn: maxAge };
+	const options = {expiresIn: maxAge};
 	return JWT.sign(payload, secret, options);
-}
-	
+};
+
 // const signRefreshToken = async (userid, maxAge = "1d") => {
 const signRefreshToken = async (userid, maxAge = "4m") => {
 	const hashedId = hashids.encode(userid);
-	const payload = {iss: "itacademy", sub: { user_id: hashedId	}};
+	const payload = {iss: "itacademy", sub: {user_id: hashedId}};
 	const secret = process.env.JWT_REFRESH_TOKEN_SECRET;
-	const options = { expiresIn: maxAge };
+	const options = {expiresIn: maxAge};
 	const token = JWT.sign(payload, secret, options);
-	const set = promisify(client.set).bind(client);
 	// await set(hashedId, token, 'EX', maxAge);
 	// await set(hashedId, token, 'EX', 24 * 60 * 60);
-	await set(hashedId, token, 'EX', 4 * 60);
-	return token
-}
-	
+	await getRedisClient().set(hashedId, token, "EX", 4 * 60);
+	return token;
+};
 
 module.exports = {
 	// generateBlob,
 	apiResponse,
 	signToken,
-	signRefreshToken
+	signRefreshToken,
 };
