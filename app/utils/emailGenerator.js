911676103d150a0cbfc0f13eff9049816c16e344
Ismael Nieto <ismael.nieto@gmail.com>
2021-07-23 12:12:12 +0200

    EmailGeneratos creates Email with components. Mail received correctly. Fake content.

--- app/utils/emailGenerator.js
+++ app/utils/emailGenerator.js
@@ -1,48 +1,46 @@
-/* eslint-disable no-useless-catch */
 const fs = require("fs");
 
-const emailGenerator = (link) => {
-	return new Promise((resolve, reject) => {
-		const path = "./app/components/";
-		let emailElements = {};
-		let email = "";
+const emailGenerator = async () => {
+	const path = "./app/components/";
+	const pathToEmail = "./app/template/";
+	let emailElements = {};
+	let buffer = "";
+	let fileContent = "";
+	let email = "";
 
-		// read all components to build Email and build an Object
-		fs.readdir(path, (err, files) => {
-			if (err) {
-				throw err;
-			}
-			files.forEach((file) => {
-				const buffer = fs.readFileSync(path + file);
-				const fileContent = buffer.toString();
-				let key = file.split(".");
-				emailElements[key[0]] = fileContent;
-			});
-		});
-
-		// read Email template and replace values through components
-		const generatedEmail = fs.readFileSync(
-			"./app/template/email_template.html",
-			"utf8",
-			(err, data) => {
-				if (err) {
-					throw err;
-				}
-				email = data;
-				for (const key in emailElements) {
-					email = email.replace("{" + key + "}", emailElements[key]);
-				}
-			}
-		);
-		//reject(false)
-		if (generatedEmail) {
-			console.log("Email was succesfully generated and returned");
-      console.log(generatedEmail);
-			resolve(generatedEmail);
-		} else {
-			reject(console.log("Email could not be generated"));
+	// read all components to build Email and build an Object
+	fs.readdir(path, (err, files) => {
+		if (err) {
+			throw err;
 		}
+		// insert filenmae as key, and the file as value
+		files.forEach((file) => {
+			buffer = fs.readFileSync(path + file);
+			fileContent = buffer.toString();
+			let key = file.split(".");
+			emailElements[key[0]] = fileContent;
+		});
 	});
+
+	// read Email template and replace values through components
+	fs.readFile(pathToEmail + "email_template.html", "utf8", (err, data) => {
+		if (err) {
+			throw err;
+		} else {
+			email = data;
+			for (const key in emailElements) {
+				email = email.replace("{{" + key + "}}", emailElements[key]);
+			}
+		}
+        fs.writeFile(pathToEmail + "email.html", email, function (err) {
+            if (err) return console.log(err);
+            console.log("EmailCreated");
+          });
+	});
+
+    const generatedEmail = fs.readFileSync(pathToEmail + "email.html",'utf8');
+    console.log("XXXX", generatedEmail)
+    return generatedEmail;
 };
 
 module.exports = emailGenerator;
