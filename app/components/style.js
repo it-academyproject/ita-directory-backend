64f4a3581ef2084bd75939bda3c2d3d9afb56f48
Ismael Nieto <ismael.nieto@gmail.com>
2021-07-23 09:54:19 +0200

    SendEmail works. Emails received, but it sends email_template without modifications

--- app/components/style.js
+++ app/components/style.js
@@ -1,3 +1,5 @@
 <style>
-    background-color: red;
-    </style>
+    body {background-color: powderblue;}
+    h1   {color: blue;}
+    p    {color: red;}
+</style>
--- app/utils/emailGenerator.js
+++ app/utils/emailGenerator.js
@@ -1,43 +1,48 @@
 /* eslint-disable no-useless-catch */
 const fs = require("fs");
 
-const emailGenerator = async (link) => {
-  // directory path
-  const path = "./app/components/";
-  let emailElements = {};
+const emailGenerator = (link) => {
+	return new Promise((resolve, reject) => {
+		const path = "./app/components/";
+		let emailElements = {};
+		let email = "";
 
-  try {
-    // read all components to build Email and build an Object
-    fs.readdir(path, (err, files) => {
-      if (err) {
-        throw err;
-      }
-      // insert filenmae as key, and the file as valu
-      files.forEach((file) => {
-        const buffer = fs.readFileSync(path + file);
-        const fileContent = buffer.toString();
-        //console.log(fileContent);
-        let key = file.split(".");
-        emailElements[key[0]] = fileContent;
-      });
-    });
-    
-    // read Email template and replace values through components
-    const emailGenerated = fs.readFile("./app/template/email_template.html", "utf8", (err, data) => {
-      let email = data;
-      for (const key in emailElements) {
-        email = email.replace('{' + key + '}', emailElements[key]);
-      }
-    });
-    // emailGenerated = "Esto es un Test";
-    
-    return emailGenerated;
+		// read all components to build Email and build an Object
+		fs.readdir(path, (err, files) => {
+			if (err) {
+				throw err;
+			}
+			files.forEach((file) => {
+				const buffer = fs.readFileSync(path + file);
+				const fileContent = buffer.toString();
+				let key = file.split(".");
+				emailElements[key[0]] = fileContent;
+			});
+		});
 
-  } catch (error) {
-    throw error;
-  }
+		// read Email template and replace values through components
+		const generatedEmail = fs.readFileSync(
+			"./app/template/email_template.html",
+			"utf8",
+			(err, data) => {
+				if (err) {
+					throw err;
+				}
+				email = data;
+				for (const key in emailElements) {
+					email = email.replace("{" + key + "}", emailElements[key]);
+				}
+			}
+		);
+		//reject(false)
+		if (generatedEmail) {
+			console.log("Email was succesfully generated and returned");
+      console.log(generatedEmail);
+			resolve(generatedEmail);
+		} else {
+			reject(console.log("Email could not be generated"));
+		}
+	});
 };
 
-
 module.exports = emailGenerator;
-
